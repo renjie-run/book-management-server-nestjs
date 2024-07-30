b62d92004d3e9d84b8cff55f571878687d2e5cd7
renjie-run <rj.aiyayao@gmail.com>
2024-07-30 18:26:23 +0800

    feat(book): add routes

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { BookService } from './book.service';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Get('list')
+  list() {
+    return '';
+  }
+
+  @Get(':id')
+  getById() {
+    return '';
+  }
+
+  @Post('create')
+  crate() {
+    return '';
+  }
+
+  @Put('update')
+  update() {
+    return '';
+  }
+
+  @Delete('delete/:id')
+  delete() {
+    return '';
+  }
 }
